9ebd209e2c0d76530ad753d5a35c05cfadc0b7e0
최선혁 <wafy75@gmail.com>
2022-11-07 11:50:45 +0900

    순수 JPA 리포지토리와 Querydsl

--- src/main/java/study/querydsl/QuerydslApplication.java
+++ src/main/java/study/querydsl/QuerydslApplication.java
@@ -1,13 +1,27 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.persistence.EntityManager;
 
 @SpringBootApplication
 public class QuerydslApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuerydslApplication.class, args);
+	}
+
+	/**
+	 * JPAQueryFactory 를 bean 으로 등록.
+	 * @param em
+	 * @return
+	 */
+	@Bean
+	JPAQueryFactory jpaQueryFactory(EntityManager em) {
+		return new JPAQueryFactory(em);
 	}
 
 }
